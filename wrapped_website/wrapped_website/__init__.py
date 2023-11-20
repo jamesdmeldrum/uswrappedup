2724ee8dc982e859e6a2856f9ce6c7836212c747
jamesdmeldrum <jmel2994@uni.sydney.edu.au>
2023-11-20 17:58:11 +1100

    hope this fixes it

--- wrapped_website/wrapped_website/__init__.py
+++ wrapped_website/wrapped_website/__init__.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-with open('../data/spotify_keys.json', 'r') as f:
+with open('./data/spotify_keys.json', 'r') as f:
           spotify_keys = json.load(f)
 os.environ["SPOTIPY_CLIENT_ID"] = spotify_keys["client_ID"]
 os.environ["SPOTIPY_CLIENT_SECRET"] = spotify_keys["client_secret"]
