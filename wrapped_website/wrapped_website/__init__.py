da0bc96b4e08c6a4dbfa8545d435379c3469dc35
jamesdmeldrum <jmel2994@uni.sydney.edu.au>
2023-11-20 18:44:58 +1100

    maybe this time?

--- wrapped_website/wrapped_website/__init__.py
+++ wrapped_website/wrapped_website/__init__.py
@@ -1,7 +1,10 @@
 import os
 import json
 
-with open('../data/spotify_keys.json', 'r') as f:
-          spotify_keys = json.load(f)
-os.environ["SPOTIPY_CLIENT_ID"] = spotify_keys["client_ID"]
-os.environ["SPOTIPY_CLIENT_SECRET"] = spotify_keys["client_secret"]
+try:
+    with open('../data/spotify_keys.json', 'r') as f:
+              spotify_keys = json.load(f)
+    os.environ["SPOTIPY_CLIENT_ID"] = spotify_keys["client_ID"]
+    os.environ["SPOTIPY_CLIENT_SECRET"] = spotify_keys["client_secret"]
+except FileNotFoundError:
+    print(os.getcwd())
